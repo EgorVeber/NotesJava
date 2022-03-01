bbcac9ed4fc297a2016b4e864fdc5c975a66f60e
JamesOhara <egor.veber.02.07.95@gmail.com>
2022-03-01 14:26:31 +0300

    Initial commit

---
+++ app/src/main/java/ru/gb/veber/homework_6_notes/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.gb.veber.homework_6_notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
